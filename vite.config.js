b3bfabf360fe9d6e793637a4598c866898486f24
Andreas Backström <mozzo242@gmail.com>
2026-02-11 19:06:05 +0100

    Change vite base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/curated-topics/',
+	base: '/',
 	plugins: [
 		preact({
 			prerender: {
